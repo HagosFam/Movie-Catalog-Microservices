dc262208f5cc893fe25ff554b26f523ead2c3e43
hagos <111482114+Kytsync@users.noreply.github.com>
2023-05-17 13:18:09 -0500

    service config update

--- movie-catelog-service/src/main/java/com/microservice/moviecatelogservice/MovieCatelogServiceApplication.java
+++ movie-catelog-service/src/main/java/com/microservice/moviecatelogservice/MovieCatelogServiceApplication.java
@@ -1,13 +1,18 @@
 package com.microservice.moviecatelogservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieCatelogServiceApplication {
+public class MovieCatelogServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatelogServiceApplication.class, args);
+	}
+
+	public void run(String...args) throws Exception {
+		System.out.println("Service first is running");
 	}
 
 }
